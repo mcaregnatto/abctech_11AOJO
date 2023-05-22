cae63bcf351f2f328e20fb405f0620ba76818875
Mateus Caregnatto <mcaregnatto@gmail.com>
2023-05-21 21:45:08 -0300

    Camada application + Dto

---
+++ src/main/java/br/com/fiap/abctechapi/application/dto/AssistResponseDto.java
@@ -0,0 +1,16 @@
+package br.com.fiap.abctechapi.application.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class AssistResponseDto {
+    @JsonProperty("assist_id")
+    private Long id;
+    private String title;
+    private String description;
+}
