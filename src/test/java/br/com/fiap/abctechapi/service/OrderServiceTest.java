916a3b22c01d7759afcb28c7c3fdf64cae846fc4
Mateus Caregnatto <mcaregnatto@gmail.com>
2023-05-24 21:19:04 -0300

    flyway_dep

--- src/test/java/br/com/fiap/abctechapi/service/OrderServiceTest.java
+++ src/test/java/br/com/fiap/abctechapi/service/OrderServiceTest.java
@@ -54,6 +54,7 @@ public class OrderServiceTest {
     private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     private final Validator validator = factory.getValidator();
 
+    //Dto restriction
     @Test
     public void testOrderDtoValidation() {
         OrderDto orderDto = new OrderDto(null, null, null, null);
@@ -66,6 +67,7 @@ public class OrderServiceTest {
         }
     }
 
+    //Dto attributes
     @Test
     public void testOrderDtoAttributes() {
         Long operatorId = 1234L;
@@ -121,14 +123,5 @@ public class OrderServiceTest {
         Assertions.assertDoesNotThrow(() -> orderService.saveOrder(newOrder, List.of(1L, 2L, 3L, 4L, 5L,6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L)));
         Mockito.verify(orderRepository, Mockito.times(1)).save(newOrder);
     }
-
-
-
-
-
-
-
-
-
 
 }
