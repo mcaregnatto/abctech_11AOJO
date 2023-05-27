f5d8f1c3d75f826f60f26fe7860ff4b69f36927b
Mateus Caregnatto <mcaregnatto@gmail.com>
2023-05-27 18:48:24 -0300

    V1_assists >15

--- src/main/resources/db/migration/V1__assists.sql
+++ src/main/resources/db/migration/V1__assists.sql
@@ -11,3 +11,18 @@ INSERT INTO assistances (name, description) VALUES ('Troca de fiação interna',
 INSERT INTO assistances (name, description) VALUES ('Manutenção em fogão', 'Reparo sem necessidade de compra de peças');
 INSERT INTO assistances (name, description) VALUES ('Manutenção em geladeira', 'Reparo sem necessidade de compra de peças');
 INSERT INTO assistances (name, description) VALUES ('Manutenção em máquina de lavar', 'Reparo sem necessidade de compra de peças');
+INSERT INTO assistances (name, description) VALUES ('Manutenção em ar condicionado', 'Reparo sem necessidade de compra de peças');
+INSERT INTO assistances (name, description) VALUES ('Instalação de chuveiro elétrico', 'Instalação de chuveiro elétrico');
+INSERT INTO assistances (name, description) VALUES ('Troca de lâmpadas', 'Substituição de lâmpadas queimadas');
+INSERT INTO assistances (name, description) VALUES ('Desentupimento de pia', 'Desentupimento de pia de cozinha ou banheiro');
+INSERT INTO assistances (name, description) VALUES ('Instalação de fechadura eletrônica', 'Instalação de fechadura eletrônica');
+INSERT INTO assistances (name, description) VALUES ('Limpeza de caixa de água', 'Limpeza e higienização de caixa de água');
+INSERT INTO assistances (name, description) VALUES ('Reparo em torneira vazando', 'Reparo em torneira que está com vazamento');
+INSERT INTO assistances (name, description) VALUES ('Instalação de ventilador de teto', 'Instalação de ventilador de teto');
+INSERT INTO assistances (name, description) VALUES ('Manutenção em aquecedor a gás', 'Reparo sem necessidade de compra de peças');
+INSERT INTO assistances (name, description) VALUES ('Desentupimento de vaso sanitário', 'Desentupimento de vaso sanitário');
+INSERT INTO assistances (name, description) VALUES ('Instalação de antena de TV', 'Instalação de antena de TV');
+INSERT INTO assistances (name, description) VALUES ('Troca de resistência de chuveiro', 'Substituição de resistência de chuveiro');
+INSERT INTO assistances (name, description) VALUES ('Reparo em torneira pingando', 'Reparo em torneira que está pingando');
+INSERT INTO assistances (name, description) VALUES ('Limpeza de ar condicionado', 'Limpeza e higienização de ar condicionado');
+INSERT INTO assistances (name, description) VALUES ('Manutenção em secadora de roupas', 'Reparo sem necessidade de compra de peças');
